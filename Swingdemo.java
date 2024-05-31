6dbe9c137c95e44dc1feb2e881fccd390d437b88
YASWANTH SEERLA <160161416+Yaswanthseerla2@users.noreply.github.com>
2024-05-31 22:05:30 +0530

    Add files via upload

---
+++ Swingdemo.java
@@ -0,0 +1,25 @@
+import java.awt.FlowLayout;
+import javax.swing.*;
+
+public class Swingdemo{
+    public static void main (String args[]){
+        JFrame f = new JFrame("User Form");
+        f.setVisible(true);
+        f.setSize(800,400);
+        f.setLayout(new FlowLayout());
+
+        JLabel l1 = new JLabel("Username");
+        f.add(l1);
+        JTextField t1 = new JTextField(20);
+        f.add(t1);
+
+        JLabel l2 = new JLabel("Password");
+        f.add(l2);
+        JTextField t2 = new JTextField(20);
+        f.add(t2);
+
+        JButton b = new JButton("Submit");
+        f.add(b);
+
+    }
+}
